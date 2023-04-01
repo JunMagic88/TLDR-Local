11f6e0c88796dacf210a9986d397a465b7faac1c
JunMagic <66362373+JunMagic88@users.noreply.github.com>
2023-04-01 14:52:24 +0800

    Update TLDR-Local.py

--- TLDR-Local.py
+++ TLDR-Local.py
@@ -1,42 +1,27 @@
-from revChatGPT.V1 import Chatbot
+from langchain import PromptTemplate, LLMChain
+from langchain.prompts import PromptTemplate
+from langchain.chat_models import ChatOpenAI
 import textwrap
-import os, re
+import os, time
 
-def start_agents():
-    # read login details from file
-    with open('agents.txt', 'r') as f:
-        data = f.readlines()
-    # create as many chatbot instances as there are logins
-    chatbots = []
-    for line in data:
-        email, password = line.strip().split(',')
-        try: 
-            chatbot = Chatbot(config={
-                "email": email,
-                "password": password
-            })
-            chatbots.append(chatbot)
-        except Exception as e: 
-            print ("Open AI login details incorrect, please check agents.txt. Ignoring line...",e)
-            continue
-    return chatbots    
+
+def summarise (text): 
+    llm = ChatOpenAI(openai_api_key="sk-xxxxxxxxxxx",temperature=0,model_name="gpt-3.5-turbo")
+    prompt = PromptTemplate(
+        input_variables=["text"],
+        template="Concisly summarise the following text and start your summary with ""---"": {text}",
+    )
+    chain = LLMChain(llm=llm, prompt=prompt)
+    summary = chain.run(text)
+    return summary    
 
 # open the file at the given filepath and return its content
 def open_file(filepath):
     with open(filepath, 'r', encoding='utf-8') as infile:
         return infile.read()
 
-# function to generate a summary of the input text using the GPT-3 model
-def summarise(bot, prompt, filename):
-        for data in bot.ask(prompt):
-            response = data["message"]
-        text = response.strip()
-        # remove extra spaces
-        text = re.sub('\s+', ' ', text)
-        return text
-            
+
 if __name__ == '__main__':
-    bots = start_agents()
 
     #set parameters
     input_directory = 'Texts'
@@ -48,7 +33,7 @@ if __name__ == '__main__':
         text = open_file(filepath)
 
         # break them down into chunks, 2000 characters each
-        chunks = textwrap.wrap(text, 2000)
+        chunks = textwrap.wrap(text, 1400,break_long_words=False)
         count = 0
         index = 0
 
@@ -57,36 +42,30 @@ if __name__ == '__main__':
         # loop over the chunks
         for chunk in chunks:
 
-            # read the prompt template and replace the placeholder with the current chunk
-            prompt = open_file('prompt.txt').replace('<<TEXT>>', chunk)
-            prompt = prompt.encode(encoding='ASCII',errors='ignore').decode()
-
             # try generate a summary through all available bot instances
             count = count + 1
             success = False
             error_not_yet_shown = True
+            retry_count = 0
             while not success:
                 try:
-                    chatbot = bots[index]
-                    if (count % 2 == 0):
-                        chatbot.clear_conversations()
-                        chatbot.reset_chat()
-                except Exception as e:
-                    print ("Can't create bot, trying again...")
-                    index = (index + 1) % len(bots)  
-                try:
-                    summary = summarise(chatbot, prompt,filename)
-                    summary = summary.replace("---", "\n\n")
+                    summary = summarise(chunk)
+                    summary = summary.replace("---", "\n- ")
                     success = True
+                    retry_count = 0 
                 except Exception as not_summarised:
+                    print(not_summarised)
                     if error_not_yet_shown: 
                         print ("Hold tight, retrying until it works...\n")
-                        error_not_yet_shown = False
-                    index = (index + 1) % len(bots)  
+                        error_not_yet_shown = False 
                     success = False
                     with open ('error_log.txt', 'w', encoding = 'utf-8') as f:
                         f.write ("Summarisation failed for: "+filename+" for the following chunk of text:\n\n\""+chunk+"\"\n\n")
                     f.close()
+                    retry_count += 1
+                if retry_count >= 5:
+                    time.sleep(30)
+                    retry_count = 0
 
             print('\n\n\n', count, 'of', len(chunks), ' - ', summary)
 
